29eb3c34c7e8ff621f12fdcdc6abb0f49f2efb43
rootRIDDICK <1613720@students.wits.ac.za>
2019-04-20 05:38:59 +0200

    added tests cases for booking classs which is the same as the one on the client side

---
+++ app/src/androidTest/java/com/example/calendar/BookingTest.java
@@ -0,0 +1,113 @@
+package com.example.calendar;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookingTest {
+
+    Booking temp;
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getName() {
+        temp=  new Booking("Tshifhiwa","","","","","","",0);
+        final String actual =temp.getName();
+        final String expected ="Tshifhiwa";
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getContact() {
+        temp=  new Booking("","","","0659545378","","","",0);
+        final String actual =temp.getContact();
+        final String expected ="0659545378";
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void getEmail() {
+        temp=  new Booking("","","","","tmavhona@gmail.com","","",0);
+        final String actual =temp.getEmail();
+        final String expected ="tmavhona@gmail.com";
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getIdentity() {
+        temp=  new Booking("","","9812176232089","","","","",0);
+        final String actual =temp.getIdentity();
+        final String expected ="9812176232089";
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void getTime() {
+        temp=  new Booking("","","","","","","08:00",0);
+        final String actual =temp.getTime();
+        final String expected ="08:00";
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void getSurname() {
+        temp=  new Booking("","Mavhona","","","","","",0);
+        final String actual =temp.getSurname();
+        final String expected ="Mavhona";
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getDate() {
+        temp=  new Booking("","","","","","2019-04-19","",0);
+        final String actual =temp.getDate();
+        final String expected ="2019-04-19";
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void empty() {
+        temp=  new Booking("Tshifhiwa","Mavhona","","0659545378","tmavhona@gmail.com","2019-04-19","08:15",0);
+        assertTrue(temp.Empty());
+    }
+
+    @Test
+    public void blocked() {
+        temp=  new Booking("Tshifhiwa","Mavhona","Admin","0659545378","tmavhona@gmail.com","2019-04-19","08:15",0);
+        assertTrue(temp.Blocked());
+    }
+
+    @Test
+    public void booked() {
+        temp=  new Booking("Tshifhiwa","Mavhona","9812176232089","0659545378","tmavhona@gmail.com","2019-04-19","08:15",0);
+        String currentUserId="9907126232089";
+        assertNotEquals("Admin",temp.getIdentity());
+        assertNotEquals("null",temp.getIdentity());
+        assertNotEquals("",temp.getIdentity());
+        assertNotEquals(currentUserId,temp.getIdentity());
+
+    }
+
+    @Test
+    public void completed() {
+        temp=  new Booking("Tshifhiwa","Mavhona","","0659545378","tmavhona@gmail.com","2019-04-19","08:15",1);
+        assertTrue(temp.Completed());
+    }
+
+    @Test
+    public void occupySlots() {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
