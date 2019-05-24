1a96e1b97a1993731be7d4f6163b42870116cad2
rootRIDDICK <1613720@students.wits.ac.za>
2019-05-24 14:06:44 +0200

    added tests cases for booking classs which is the same as the one on the client side

---
+++ app/src/test/java/com/example/calendar/WeekViewTest.java
@@ -0,0 +1,40 @@
+package com.example.calendar;
+
+import android.content.Intent;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+import static org.junit.Assert.*;
+@RunWith(RobolectricTestRunner.class)
+public class WeekViewTest {
+
+
+    WeekView myHome;
+
+    @Before
+    public void setUp() throws Exception {
+        Intent intent = new Intent(RuntimeEnvironment.application, HomeScreen.class);
+        String temp="20190527";
+        intent.putExtra("Month", "June");
+        intent.putExtra("CurrentDate", temp);
+        intent.putExtra("CheckedDate", temp);
+        myHome = Robolectric.buildActivity(WeekView.class, intent).setup().get();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void check() {
+        assertNotNull(myHome);
+    }
+
+}
